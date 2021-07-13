a75a9aa4d3f854d9a56121a38fc960407959aef1
y43d <philanderson556@gmail.com>
2021-07-13 10:00:01 +0100

    initial commit

---
+++ hello-world/target/rls/debug/deps/hello_world-846af7de6fbd1593.d
@@ -0,0 +1,5 @@
+c:\Users\Philip\Documents\PHILIP\Programming\Rust\hello-world\target\rls\debug\deps\hello_world-846af7de6fbd1593.rmeta: src\main.rs
+
+c:\Users\Philip\Documents\PHILIP\Programming\Rust\hello-world\target\rls\debug\deps\hello_world-846af7de6fbd1593.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world/target/rls/debug/deps/hello_world-c179475bfa230366.d
@@ -0,0 +1,5 @@
+c:\Users\Philip\Documents\PHILIP\Programming\Rust\hello-world\target\rls\debug\deps\hello_world-c179475bfa230366.rmeta: src\main.rs
+
+c:\Users\Philip\Documents\PHILIP\Programming\Rust\hello-world\target\rls\debug\deps\hello_world-c179475bfa230366.d: src\main.rs
+
+src\main.rs:
