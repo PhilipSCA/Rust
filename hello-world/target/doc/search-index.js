240346e109de124f5321ae86b1135c4b4d3b5762
y43d <philanderson556@gmail.com>
2021-07-13 10:09:22 +0100

    remove doc

--- hello-world/target/doc/search-index.js
+++
@@ -1,4 +0,0 @@
-var searchIndex = JSON.parse('{\
-"hello_world":{"doc":"","t":[5],"n":["main"],"q":["hello_world"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
-}');
-if (window.initSearch) {window.initSearch(searchIndex)};
--- hello-world/target/doc/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["hello_world"] = {"name":"","files":["main.rs"]};
-createSourceSidebar();
